f27200803b72d1ea4103f5225df934f452b31c93
Helene Coullon <helene.coullon@inria.fr>
2024-09-02 10:43:12 +0200

    update movie.py

--- movie/movie.py
+++ movie/movie.py
@@ -1,5 +1,4 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
-from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, request, jsonify
 
 import resolvers as r
@@ -10,19 +9,12 @@ app = Flask(__name__)
 
 # todo create elements for Ariadne
 
-
 # root message
 @app.route("/", methods=['GET'])
 def home():
     return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
 
-#####
 # graphql entry points
-
-@app.route('/graphql', methods=['GET'])
-def playground():
-    return PLAYGROUND_HTML, 200
-    
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
     # todo to complete
