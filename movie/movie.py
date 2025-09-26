c3749d2bfa4943edefd481cc149838dc383fe470
Thomas Batista <thomas.batista@etu.univ-amu.fr>
2025-09-26 16:16:14 +0200

    Init movie graphql API

--- movie/movie.py
+++ movie/movie.py
@@ -1,5 +1,5 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -7,7 +7,21 @@ PORT = 3001
 HOST = '0.0.0.0'
 app = Flask(__name__)
 
-# todo create elements for Ariadne
+type_defs = load_schema_from_path('movie.graphql')
+
+query = QueryType()
+query.set_field('movie_with_id', r.movie_with_id)
+query.set_field('actor_with_id', r.actor_with_id)
+
+actor = ObjectType('Actor')
+actor.set_field('movies', r.resolve_movies_in_actor)
+movie = ObjectType('Movie')
+movie.set_field('actors', r.resolve_actors_in_movie)
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 
 # root message
 @app.route("/", methods=['GET'])
@@ -17,7 +31,15 @@ def home():
 # graphql entry points
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
